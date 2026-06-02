fb1daefc156a898e42406792fdab17d4cacb973f
Endg4meZer0 <65147948+Endg4meZer0@users.noreply.github.com>
2026-06-03 06:22:08 +1000

    lab2-5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './CSS/App.css';
-import buildings from './data.js';
-import Table from './components/Table.js';
+import "./App.css";
+import Chart from "./components/Chart";
+import Table from "./components/Table";
+import buildings from "./buildings";
 
 function App() {
   return (
     <div className="App">
       <h3>Самые высокие здания и сооружения</h3>
-      <Table data={buildings} amountRows="15" usePagination={true} />
+
+      <Chart data={buildings} />
+
+      <Table data={buildings} amountRows={100} />
     </div>
   );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,62 +1,33 @@
-import { useState } from 'react';
-import TableHead from './TableHead.js';
-import TableBody from './TableBody.js';
-import Filter from './Filter.js';
+import React from "react";
 
-/*
-  компонент, выводящий на страницу таблицу с фильтрацией и пагинацией
-  props:
-  data - данные для таблицы в виде массива объектов
-  amountRows - количество строк на странице
-  usePagination - true/false, выводить ли блок пагинации
-*/
-const Table = (props) => {
-  const [activePage, setActivePage] = useState(1);
-  const [dataTable, setDataTable] = useState(props.data);
-
-  const amountRows = Number(props.amountRows);
-  const usePagination = props.usePagination === true || props.usePagination === 'true';
-  const pagesCount = Math.ceil(dataTable.length / amountRows);
-  const pages = Array.from({ length: pagesCount }, (_, index) => index + 1);
-
-  const updateDataTable = (value) => setDataTable(value);
-  const resetPage = () => setActivePage(1);
-
-  const changeActive = (event) => {
-    setActivePage(Number(event.target.textContent));
-  };
+const Table = ({ data, amountRows }) => {
+  const rowsToShow = data.slice(0, amountRows);
 
   return (
-    <>
-      <h4>Фильтры</h4>
-      <Filter filtering={updateDataTable} fullData={props.data} resetPage={resetPage} />
-
-      <table>
-        <TableHead head={Object.keys(props.data[0])} />
-        <TableBody
-          body={dataTable}
-          amountRows={amountRows}
-          numPage={activePage}
-          usePagination={usePagination}
-        />
-      </table>
-
-      {dataTable.length === 0 && <p className="empty">Нет данных по заданным фильтрам</p>}
-
-      {usePagination && pagesCount > 1 && (
-        <div className="pagination">
-          {pages.map((item) => (
-            <span
-              key={item}
-              onClick={changeActive}
-              className={activePage === item ? 'activePage' : ''}
-            >
-              {item}
-            </span>
-          ))}
-        </div>
-      )}
-    </>
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Название</th>
+          <th>Тип</th>
+          <th>Страна</th>
+          <th>Город</th>
+          <th>Год</th>
+          <th>Высота (м)</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rowsToShow.map((building, index) => (
+          <tr key={index}>
+            <td>{building["Название"]}</td>
+            <td>{building["Тип"]}</td>
+            <td>{building["Страна"]}</td>
+            <td>{building["Город"]}</td>
+            <td>{building["Год"]}</td>
+            <td>{building["Высота"]}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
